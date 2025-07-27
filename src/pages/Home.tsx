c3596ea8eff4a01099de57f47f671f2fc2f029a5
Sam Caldwell <github@samcaldwell.net>
2025-07-26 20:28:02 -0500

    Add modular components and dynamic CNAME

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// (c) 2025 Asymmetric Effort, LLC. All Rights Reserved.
+import React from 'react';
+
+/** Display the landing page content. */
+export default function Home() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are Asymmetric Effort.</p>
+    </div>
+  );
+}
