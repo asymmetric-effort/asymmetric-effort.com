3693c4ff7fff0b792305b8b76ad796b7bad4bd25
sam-caldwell <sam-caldwell@users.noreply.github.com>
2026-04-26 14:23:07 +0000

    deploy: dab08b7d11b70b726760e9482b65b79312c0e17f

--- dist/components/Header.js
+++
@@ -1,7 +0,0 @@
-// (c) 2025 Asymmetric Effort, LLC. All Rights Reserved.
-import React from 'react';
-/** Application header displaying the logo. */
-export default function Header() {
-    return (React.createElement("header", { className: "header" },
-        React.createElement("img", { src: "logo.png", alt: "logo", className: "logo" })));
-}
--- dist/projects/GreyNet.js
+++
@@ -1,8 +0,0 @@
-// (c) 2025 Asymmetric Effort, LLC. All Rights Reserved.
-import React from 'react';
-/** Info page for the GreyNet project. */
-export default function GreyNet() {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "GreyNet"),
-        React.createElement("p", null, "A decentralized zero-trust product for the rest of us.")));
-}
